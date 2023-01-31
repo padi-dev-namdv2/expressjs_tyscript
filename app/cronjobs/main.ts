c6965165c02459549046e7325c9e8b9c3d0c3efb
padi-dev-namdv2 <122244639+padi-dev-namdv2@users.noreply.github.com>
2023-01-31 14:14:01 +0700

    Update main.ts

--- app/cronjobs/main.ts
+++ app/cronjobs/main.ts
@@ -1,6 +1,8 @@
 import { exampleCronJob } from "./exampleCronJob";
+import { testCronJob } from "./testCronJob";
 
 export const cronJob = () => {
     console.log("Chạy cron job");
     exampleCronJob.start();
+    testCronJob.start();
 }
